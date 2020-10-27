07ee4ccad32284c030a213e6f798f8c17a70c769
unknown <daviddhrasmussen@gmail.com>
2020-10-27 15:08:20 +0100

    struktur på read.c program

---
+++ src/read.c
@@ -0,0 +1,23 @@
+
+void start_read(){
+
+    // Open a file (Keywords, Requirments, Long CV)
+    // Send file to read_file function
+    // Send Long CV file to tag_searcher
+
+}
+
+
+void word_reader(){
+
+    // Read every word of a file or section (in an array)
+    // Find tags, and end current reading there
+
+}
+
+
+void tag_searcher(){
+
+    // Use to divide sections in Long CV reading (array)
+
+}
