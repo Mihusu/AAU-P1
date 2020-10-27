a2dabb182538851da7f5eb7df7e32f2ca680399f
unknown <daviddhrasmussen@gmail.com>
2020-10-27 16:56:16 +0100

    1 try

--- src/read.c
+++ src/read.c
@@ -1,16 +1,55 @@
 
-void start_read(){
+#include<stdio.h>
 
-    // Open a file (Keywords, Requirments, Long CV)
-    // Send file to word_reader function
-    // Send Long CV file to tag_searcher
+void start_read();
+void word_reader();
+void tag_searcher();
+
+int main(void){
+
+char wow[10];
+
+    start_read();
 
 }
 
+void start_read(){
+    // Open a file (Keywords, Requirments, Long CV)
+    FILE *hello; 
+    hello = fopen("Hello.txt", "r");
 
-void word_reader(){
+    word_reader(hello);
 
+    // Send Long CV file to tag_searcher
+    if(Long CV){
+        tag_searcher
+    }
+    // Send file to word_reader function
+    else{
+    
+        word_reader();
+    }
+    
+} 
+
+
+void word_reader(FILE *hitler){
     // Read every word of a file or section (in an array)
+    char tom[100];
+    char *jim[100];
+    int hej = 0;
+    while(1){
+        fscanf(hitler, "%s", tom);
+        jim[hej]
+
+    
+
+    
+    }
+    if(){
+
+    }
+
     // Find tags, and end current reading there
 
 }
