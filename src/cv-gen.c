97c22b1fbfe970370c2a50a9188378f172a0b109
Sir Godly Hans Heje <hansheje@hotmail.com>
2020-10-28 15:05:00 +0100

    added Paragraph_match, to determin match amount

--- src/cv-gen.c
+++ src/cv-gen.c
@@ -2,35 +2,43 @@
 #include <stdbool.h>
 #include <string.h>
 
+int CV_LENGTH = 5;
+int KEYWORD_LENGTH = 5;
 int max_word_count = 700;
 int min_word_count = 500;  //change at a later stage, to the req.txt
 
-bool is_word_match(char word_CV[], char word_Key[]);
+bool is_word_match(char word_1[], char word_2[]);
+int paragraph_Matches(char Paragraph[][50], char Keywords[][50]);
+int paragraph_Length(char paragraph[][50]);
+int paragraph_Importance(char paragraph[][]);
+
 
 int main(void){
-    //load largecv.read.c
-    //load req.read.c
-    //load keyword.read.c
-    //run word_matcher, to see how many words match keywords
-
-    printf("\n\n=====start GEN=====\n"); //debug
-    char cv[] = "hej";
-    char buzz[] = "hej";
-
-    printf("Wordmatch value: %d",is_word_match(cv,buzz)); //debug
-    printf("\n=====end GEN=====\n\n"); //Debug
+    char cv[4][50] = {"hej","hvordan","gaar","det"};
+    char buzz[3][50] = {"hej","hvordan","gaar"};
+    printf("%d",paragraph_Matches(cv,buzz));
     return 0;
 }
-/*
- * Function to compare words
- */
 
-bool is_word_match(char word_CV[], char word_Key[]){
-    printf("-----is_Word_Match Start-----\n"); //debug
-    printf("word_CV = %s, word_Key = %s\n",word_CV,word_Key); //debug
+//Checks for how many times a paragraph matches keywords
+int paragraph_Matches(char Paragraph[][50], char Keywords[][50]){    
+    int match_Weight = 0;
+    for(int j = 0; j < 3; j++)  //need sizeof command instead of 3 //buzz length
+    {  
+        int i = 0;
+        for(; i < 4; i++) //need sizeof instead of 4, length of cv
+        {
+            printf("cv[%d]: %s, buzz[%d]: %s\n ",i,Paragraph[i],j,Keywords[j]);
+            match_Weight += is_word_match(Paragraph[i],Keywords[j]);
+        }
+    }
+    return(match_Weight);
+}
+
+//compares for string match, to see if they are the same.
+bool is_word_match(char word_1[], char word_2[]){
     bool word_Match;
-    word_Match = strcmp(word_CV,word_Key); //compares for string match
-
+    word_Match = strcmp(word_1,word_2); 
     //flips the bool value, since strcmp = 0 is true; strcmp = 1 is false
     if(word_Match == 0){ 
         word_Match = 1;
@@ -43,34 +51,10 @@ bool is_word_match(char word_CV[], char word_Key[]){
         return(word_Match);
     }
 }
-//
-///*
-// * Function to calculate "weight" of paragraph
-// */
-//int weight(paragraph, keywords){
-//    if (word_matcher(word1, word2) {
-//        counter++
-//    }
-//    return counter;
-//}
-//
-///*
-// * Function to count the words of a paragraph
-// */
-//int word_count(paragraph){
-//    return number_of_words;
-//}
-//
-///*
-// * Function to create an array with the importance and length of all paragraphs
-// */
-//void importance(long_cv, *importance_array){
-//    for (int i = 0; i < size(long_cv); i++) {
-//        int count = word_count(paragraph);
-//        *importance_array.i = (weight(paragraph) / count, count);
-//    }
-//}
-//
+
+
+
+
 ///*
 // * Function to create an array with booleans of wether a paragraph is to be
 // * included or not
