0cae2f7c2aff33875611bd432a270100ad3504a5
Mikkel Kaa <s164152@student.dtu.dk>
2020-10-28 17:33:11 +0100

    Not ready to test yet, read.c, Mikkel

--- src/read.c
+++ src/read.c
@@ -1,61 +1,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void start_read();
 void word_reader();
 void tag_searcher();
+void arrayExtender();
 
 int main(void){
 
-char wow[10];
 
     start_read();
 
+    return 0;
 }
 
 void start_read(){
+    char **toOut_pp;
     // Open a file (Keywords, Requirments, Long CV)
     FILE *hello; 
     hello = fopen("Hello.txt", "r");
 
-    word_reader(hello);
+    word_reader(hello, &toOut_pp);
 
     // Send Long CV file to tag_searcher
-    if(Long CV){
-        tag_searcher
+    if(0){
+        //tag_searcher
     }
     // Send file to word_reader function
     else{
     
-        word_reader();
+        //word_reader();
     }
     
 } 
 
 
-void word_reader(FILE *hello2){
+void word_reader(FILE *hello2, char ***theOutput_ppp){
     // Read every word of a file or section (in an array)
-    char arrayofwords[9999];
-    char *jim[9999];
-    int hej = 0, i;
+    char **allWords_pp = malloc(1 * (__SIZEOF_POINTER__));
+    int hej = 0;
     
     while(1){
-        fscanf(hello2, "%s", &arrayofwords[hej]);
-        hej++;
-
-    
-    }
+        if(feof(hello2)){
+            // feof() -> is next char in the file_input-stream the 'EOF' (End Of File)
+            break;
+        }
+        // Temporary storage for 1 word.
+        char theWordTemp_p[52]; // Longest word in Danish is 51
+        fscanf(hello2, "%s", theWordTemp_p);
+        // Final storage for 1 word, only allocated the required length for that word
+        char theWord_p[] = malloc((strlen(theWordTemp_p) + 1) * sizeof(char));
+        strcpy(theWord_p, theWordTemp_p);
         
-
-    if(File contains no more words)
-
+        allWords_pp[hej] = theWord_p;
+        arrayExtender(&allWords_pp, hej+1);
+        hej++;
     }
 
     // Find tags, and end current reading there
-
+    *theOutput_ppp = allWords_pp;
 }
 
+void arrayExtender(char ***theIn_ppp, int currLength){
+    // Adds an extra entry to the array
+    char *theNew_pp[] = malloc((currLength + 1) * (__SIZEOF_POINTER__));
+    for(int i = 0; i < currLength; i++){
+        theNew_pp[i] = (*theIn_ppp)[i];
+    }
+    free(*theIn_ppp);
+    *theIn_ppp = theNew_pp;
+}
 
 void tag_searcher(){
 
