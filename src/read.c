b8f4b99e2a20b299a7f19654959a4018665dafd4
Mikkel Kaa <s164152@student.dtu.dk>
2020-10-29 14:50:05 +0100

    Starting on void pointers

--- src/read.c
+++ src/read.c
@@ -7,19 +7,19 @@ void start_read();
 void word_reader();
 void tag_searcher();
 void arrayExtender();
+void** arrayExtenderExperimental();
 
 
 int main(void){
 
     char **keaywords_pp, ***inCVSections_ppp, ***itemicedSections_ppp;
-    int wordsInKeaywords, *wordsInSections_p, *itemsInItemicasion_p;
-    start_read(&keaywords_pp, &wordsInKeaywords, &inCVSections_ppp, &wordsInSections_p, &itemicedSections_ppp, &itemsInItemicasion_p);
+    start_read(&keaywords_pp, &inCVSections_ppp, &itemicedSections_ppp);
 
     return 0;
 }
 
 
-void start_read(char ***theKeaywords_ppp, int *wordsInKeaywords_p, char ****cvLongSections_pppp, int **cvLongSectionsWord_pp, char ****cvLongItemiced_pppp, int **cvLongItemicedWord_pp){
+void start_read(char ***theKeaywords_ppp, char ****cvLongSections_pppp, char ****cvLongItemiced_pppp){
     // Open a file (Keywords, Long CV)
     /*
     FILE *hello; 
@@ -28,7 +28,7 @@ void start_read(char ***theKeaywords_ppp, int *wordsInKeaywords_p, char ****cvLo
     word_reader(fopen("Hello.txt", "r"), theKeaywords_ppp);
 
     // Send Long CV file to tag_searcher
-    tag_searcher(fopen("LongCV.txt", "r"), );
+    tag_searcher(fopen("LongCV.txt", "r"), cvLongItemiced_pppp, cvLongSections_pppp);
     
     // Send file to word_reader function
     
@@ -54,6 +54,7 @@ void word_reader(FILE *theInFile, char ***theOutput_ppp){
         
         allWords_pp[hej] = theWord_p;
         arrayExtender(&allWords_pp, hej+1);
+        //allWords_pp = (char **) arrayExtenderExperimental(allWords_pp, hej+1);
         hej++;
     }
 
@@ -64,7 +65,7 @@ void word_reader(FILE *theInFile, char ***theOutput_ppp){
 
 void arrayExtender(char ***theIn_ppp, int currLength){
     // Adds an extra entry to the array
-    char *theNew_pp[] = malloc((currLength + 1) * (__SIZEOF_POINTER__));
+    char **theNew_pp = malloc((currLength + 1) * (__SIZEOF_POINTER__));
     for(int i = 0; i < currLength; i++){
         theNew_pp[i] = (*theIn_ppp)[i];
     }
@@ -73,14 +74,28 @@ void arrayExtender(char ***theIn_ppp, int currLength){
 }
 
 
-void tag_searcher(){
+void** arrayExtenderExperimental(void **theIn_pp, int currLength){
+    // Creates a new array (containing pointers) thats 1 space longer
+    // than the old array, copies the content of old into new and
+    // free the old array.
+    void **theNew_pp = malloc((currLength + 1) * (__SIZEOF_POINTER__));
+    for(int i = 0; i < currLength; i++){
+        theNew_pp[i] = theIn_pp[i];
+    }
+    free(theIn_pp);
+    return theNew_pp;
+}
+
+
+void tag_searcher(FILE *fileInbound, char ****hellos_pppp, char ****hellos2_pppp){
 
     // Use to divide sections in Long CV reading (array) 
     
     // Run Word_Reader on Long CV
-    word_reader();
+    while(!(feof(fileInbound))){
+        word_reader();
+    }
     
-
 }
 
 void line_reader(){
@@ -88,7 +103,7 @@ void line_reader(){
 
 }
 
-/*
+/* Exampel of LongCV conntent:
 #Workexperience
 2009-2011 Flaskedreng i Netto.
 2011-2013 Flymekaniker.
@@ -104,7 +119,4 @@ Going home is for the weak.
 
 I am very proud.
 I am also happy.
-
-WorkExperience
-[0,1]
 // */
