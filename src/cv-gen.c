c1c46c221c13c0fbfa9878970f869bba9066970f
Sir Godly Hans Heje <hansheje@hotmail.com>
2020-10-29 11:26:36 +0100

    Density array working, running through  paragraphs

--- src/cv-gen.c
+++ src/cv-gen.c
@@ -3,36 +3,48 @@
 #include <string.h>
 
 #define MAX_LETTERS 50 //PARAGRAPH_MAX_LETTERS_PER_WORD
-#define KEYWORD_LENGTH 6 //amount of keywords in array
-#define PARA_AMOUNT 5 //amount of paragraphs
-#define PARA_LENGTH 8 //length of each paragraph
+#define KEYWORD_LENGTH 12 //amount of keywords in array
+#define PARA_AMOUNT 6 //amount of paragraphs
+#define PARA_LENGTH 12 //length of each paragraph
 
 bool is_word_match(char word_1[], char word_2[]);
 int paragraph_Weight(char Paragraph[][MAX_LETTERS], char Keywords[][MAX_LETTERS], int length);
 void paragraph_Density(int Weight, int Length,double *density);
 
 int main(void){
-    char paragraph1[PARA_LENGTH][MAX_LETTERS] = {"hej","hvordan","gaar","det","asd","Jeg","Har","BSC"}; //testing, tb replace by read.c
-    char buzz[KEYWORD_LENGTH][MAX_LETTERS] = {"hej","hvordan","gaar","det","med","sig"}; //testing, tb replace by read.c
-    int length[PARA_AMOUNT] = {PARA_LENGTH,2,3,4,5}; //testing, tb replace by read.c
-    double density_of_Paragraph[PARA_AMOUNT];
+    char CV[PARA_AMOUNT][PARA_LENGTH][MAX_LETTERS] = {{"Jeg","har","en","gym","uddannelse"}, //testing, tb replace by read.c
+                                            {"jeg","har","arbejdet","i","netto"},
+                                            {"Jeg","er","god","til","C","prog"},
+                                            {"Jeg","kan","finde","ud","af","machinelearning","og","statistik","og","sandsynlighedsteori"},
+                                            {"Jeg","tager","en","Bsc","i","Software"},
+                                            {"Erfaring","med","C","python","css","og","databaser"}};
     
+    char buzz[KEYWORD_LENGTH][MAX_LETTERS] = {"netto","gym","C","prog","css","databaser","python","statistik","sandsynlighedsteori","machinelearning","Bsc","Software"}; //testing, tb replace by read.c
+    int length[PARA_AMOUNT] = {5,5,6,10,6,7}; //length of each individuel paragraph //testing, tb replace by read.c
+    double density_of_Paragraph[PARA_AMOUNT];
 
-    for (int i = 0; i < PARA_AMOUNT; i++) //loops through all paragraphs to get each density. only the first paragraph
-    {
-        int weight = paragraph_Weight(paragraph1,buzz,length[i]);
+    for (int i = 0; i < PARA_AMOUNT; i++) //loops through all paragraphs to get each density.
+    {   
+        int weight = paragraph_Weight(CV[i],buzz,length[i]);
         paragraph_Density(weight,length[i],&density_of_Paragraph[i]);
-        printf("density of paragraph %d: %lf",i, density_of_Paragraph[i]);
+        printf("density of paragraph %d: %lf",i+1, density_of_Paragraph[i]);
     }
     return 0;
 }
 
 //calculates the density of all paragraphs and an returns the value
-//void density_of_All_Paragraphs(){}
+//void cv_Density(char CV[][PARA_LENGTH][MAX_LETTERS], double* density_of_Paragraph[PARA_AMOUNT],int length[PARA_AMOUNT],char buzz[KEYWORD_LENGTH][MAX_LETTERS]){
+//    for (int i = 0; i < PARA_AMOUNT; i++) //loops through all paragraphs to get each density. only the first paragraph
+//    {   
+//        int weight = paragraph_Weight(CV[i],buzz,length[i]);
+//        paragraph_Density(weight,length[i],density_of_Paragraph[i]);
+//        printf("density of paragraph %d: %lf",i+1, density_of_Paragraph[i]);
+//    }
+//}
 
 // divies paragraph weight with the same paragraphs length, to find density form 0 to 1
 void paragraph_Density(int Weight, int Length, double* density){
-    printf("    weight: %d, length: %d\n", Weight, Length);
+    printf("\nweight: %d, length: %d\n", Weight, Length);
     *density = ((double)Weight)/((double)Length);
 }
 
