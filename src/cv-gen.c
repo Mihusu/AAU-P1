64574383a73894eb9c1aeaa2f7660c6f481814d7
Mihusu <ming2200@hotmail.com>
2020-10-29 12:40:12 +0100

    inclusion function

--- src/cv-gen.c
+++ src/cv-gen.c
@@ -1,73 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-int max_word_count = 700;
-int min_word_count = 500;  //change at a later stage, to the req.txt
+void inclusion(double *a, int b, bool *c);
 
 int main(void){
-    //load largecv.read.c
-    //load req.read.c
-    //load keyword.read.c
-
-    //run word_matcher, to see how many words match keywords
+    int b;
+    double strArr_density_CV[] = {8, 32, 4556, 3, 93, 43, 112, 342};
+    inclusion(strArr_density_CV, b);
 
     return 0;
 }
-/*
- * Function to compare words
- */
 
-bool is_word_match(word1, word2){
-
-    return;
+int cmpfunc (const void *ep1, const void *ep2) {
+    double *tp1 = (double*)ep1;
+    double *tp2 = (double*)ep2;
+    
+    if (*tp1 < *tp2) {
+        return -1;
+    }
+    else if (*tp1 < *tp2) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
-//
-///*
-// * Function to calculate "weight" of paragraph
-// */
-//int weight(paragraph, keywords){
-//    if (word_matcher(word1, word2) {
-//        counter++
-//    }
-//    return counter;
-//}
-//
-///*
-// * Function to count the words of a paragraph
-// */
-//int word_count(paragraph){
-//    return number_of_words;
-//}
-//
-///*
-// * Function to create an array with the importance and length of all paragraphs
-// */
-//void importance(long_cv, *importance_array){
-//    for (int i = 0; i < size(long_cv); i++) {
-//        int count = word_count(paragraph);
-//        *importance_array.i = (weight(paragraph) / count, count);
-//    }
-//}
-//
-///*
-// * Function to create an array with booleans of wether a paragraph is to be
-// * included or not
-// */
-//void include(*importance_array, *inclusion_array){
-//    *inclusion_array.i = true;// sets entries to true if they should be included
-//}
-//
-///*
-// * Function to output the plain text file with the final cv, so it can be
-// * formatted
-// */
-//void output(long_cv, *inclusion_array){
-//    for (int i = 0; i < size(long_cv); i++) {
-//        if (*inclusion_array = true) {
-//            print long_cv.i;
-//        }
-//    }
-//}
-//
-//
+
+void inclusion(double *a, int max_word_count_CV = 702, bool *c) {
+    typedef struct {
+        char strVal;
+        int intval;
+    } tTuple;
+    // tTuple list[702];
+
+    int biggist_density, sum = 0;
+    int max_word_count_CV = 702; //change at a later stage, to the req.txt
+    int j, i; 
+
+    while(sum < max_word_count_CV) {
+        for(i = 0; i < max_word_count_CV; i++) {
+            sum += i;
+        
+        }
+        
+        for(j = 0; j < biggist_density; i++)  {
+                
+        }
+    } 
+    printf("AFTER sorting\n"); 
+    qsort(a, 702, sizeof(int), cmpfunc);
+    // printing strings in sorted order 
+    for (i= 0; i < 702; i++) {
+        printf(" %s ", a); 
+    }
+} 
+
+/* Første er til at vælge de vigtigste fra string arrays 
+   (KOMMER FRA DESENTY FUNKTIONEN). Skal sortere og gå tilbage og sortere igen, 
+   anden skal angive den maksimale længde af et CV og det tredje skal gøres ved at vælge afsnit
+   og sortere dem fra ved hjælp af TRUE eller FALSE. */
