bba3b00feba58acbcd9575071e1a943bf7b7cb04
Mikkel Kaa <s164152@student.dtu.dk>
2020-11-02 10:03:58 +0100

    Fix to git problem again

--- src/read.c
+++ src/read.c
@@ -111,14 +111,6 @@ void line_reader(){
 
     // Use to divide sections in Long CV reading (array)
 
-void arrayExtender(char ***theIn_ppp, int currLength){
-    // Adds an extra entry to the array
-    char **theNew_pp = malloc((currLength + 10) * sizeof(char *));
-    for(int i = 0; i < currLength; i++){
-        theNew_pp[i] = (*theIn_ppp)[i];
-    }
-    free(*theIn_ppp);
-    *theIn_ppp = theNew_pp;
 }
 
 /* Exampel of LongCV conntent:
@@ -131,51 +123,6 @@ void arrayExtender(char ***theIn_ppp, int currLength){
 #FreeText
 Hello I am a good worker.
 
-void** arrayExtenderExperimental(void **theIn_pp, int currLength){
-    // Creates a new array (containing pointers) thats 1 space longer
-    // than the old array, copies the content of old into new and
-    // free the old array.
-    void **theNew_pp = malloc((currLength + 1) * sizeof(void *));
-    for(int i = 0; i < currLength; i++){
-        theNew_pp[i] = theIn_pp[i];
-    }
-    free(theIn_pp);
-    return theNew_pp;
-}
-
-
-void tag_searcher(FILE *fileInbound, char ****theItems_pppp, char ****theText_pppp){
-    char ***theReadItems_ppp, ***theReadText_ppp;
-    theReadItems_ppp = malloc(2 * sizeof(char **));
-    theReadText_ppp = malloc(1 * sizeof(char **));
-    // Use to divide sections in Long CV reading (array) 
-    
-    // Run Word_Reader on Long CV
-    while(!(feof(fileInbound))){
-        //word_reader();
-    }
-    
-}
-
-void line_reader(){
-
-
-}
-
-/* Exampel of LongCV conntent:
-#Workexperience
-2009-2011 Flaskedreng i Netto.
-2011-2013 Flymekaniker.
-#Education
-2016 AAU Batchelor i software.
-2013 HTX-Hillerød, Mat-Fys.
-#FreeText
-Hello I am a good worker.
-I flush the toilet always.
-Going home is for the weak.
-I am very proud.
-I am also happy.
-// */
 I flush the toilet always.
 
 Going home is for the weak.
