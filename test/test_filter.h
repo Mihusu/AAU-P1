e60c30855a4ebed6e959c166d931bdcf5f80cc7f
Kristoffer Bach Wilhjelm <kwilhj20@student.aau.dk>
2020-11-03 11:24:23 +0100

    Completed renaming.

--- test/test_filter.h
+++
@@ -1,7 +0,0 @@
-#include "CuTest.h"
-
-void test_is_word_match_true(CuTest *tc);
-void test_is_word_match_false(CuTest *tc);
-void test_is_word_match_fails(CuTest *tc);
-
-CuSuite* cv_gen_get_suite();
