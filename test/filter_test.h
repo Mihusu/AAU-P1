468bc18bb4f2c461864994e9f09e481d68a953e3
Kristoffer Bach Wilhjelm <kwilhj20@student.aau.dk>
2020-11-05 13:02:35 +0100

    Included tests in suite and added include guards.

--- test/filter_test.h
+++ test/filter_test.h
@@ -1,7 +1,14 @@
+#ifndef P1_TEST_FILTER_TEST_H
+#define P1_TEST_FILTER_TEST_H
+
 #include "CuTest.h"
 
 void test_is_word_match_true(CuTest *tc);
 void test_is_word_match_false(CuTest *tc);
 void test_is_word_match_fails(CuTest *tc);
+void test_CV_Density(CuTest *tc);
+void test_Inclusion(CuTest *tc);
 
-CuSuite* cv_gen_get_suite();
+CuSuite *filter_get_suite();
+
+#endif  // P1_TEST_FILTER_TEST_H
