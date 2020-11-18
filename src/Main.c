a241aefe467a3261403239655fee5013829b743f
Hans434 <hansheje@hotmail.com>
2020-11-18 10:52:53 +0100

    Added main file

---
+++ src/Main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "filter.c"
+
+#define KEYWORD_LENGTH 12 //amount of keywords in array
+#define PARA_AMOUNT 6 //amount of paragraphs
+#define PARA_LENGTH 12 //length of each paragraph
+#define MAX_WORDS 10 //max words in the CV
+#define OUTPUT_PARA_AMOUNT 2 //amount of paragraphs in output
+
+int main(void){
+    printf("########  ########   #######        ## ########  ######  ########    ##     ## #### ########    ###\n##     ## ##     ## ##     ##       ## ##       ##    ##    ##       ##     ##  ##     ##      ## ##   \n##     ## ##     ## ##     ##       ## ##       ##          ##       ##     ##  ##     ##     ##   ##  \n########  ########  ##     ##       ## ######   ##          ##       ##     ##  ##     ##    ##     ## \n##        ##   ##   ##     ## ##    ## ##       ##          ##        ##   ##   ##     ##    ######### \n##        ##    ##  ##     ## ##    ## ##       ##    ##    ##         ## ##    ##     ##    ##     ## \n##        ##     ##  #######   ######  ########  ######     ##          ###    ####    ##    ##     ## \n");
+    //char **cv[PARA_AMOUNT];
+    //char **cv1[];
+    char *cv[PARA_AMOUNT][PARA_LENGTH] = {{"Jeg","har","en","gym","uddannelse"}, //testing, tb replace by read.c
+                                            {"jeg","har","arbejdet","i","netto"},
+                                            {"Jeg","er","god","til","C","prog"},
+                                            {"Jeg","kan","finde","ud","af","machinelearning","og","statistik","og","sandsynlighedsteori"},
+                                            {"Jeg","tager","en","Bsc","i","Software"},
+                                            {"Erfaring","med","C","python","css","og","databaser"}};
+    char *buzz[KEYWORD_LENGTH] = {"netto","gym","C","prog","css","databaser","python","statistik","sandsynlighedsteori","machinelearning","Bsc","Software"}; //testing, tb replace by read.c
+    int length[PARA_AMOUNT] = {5,5,6,10,6,7}; //length of each individuel paragraph //testing, tb replace by read.c
+    double density_of_paragraph[PARA_AMOUNT] = {};
+    calculate_cv_density(density_of_paragraph,cv,buzz,length);
+    bool included_paragraphs[PARA_AMOUNT] = {};
+    include_paragraph(density_of_paragraph,cv,length,included_paragraphs);
+    char *filtered_cv[OUTPUT_PARA_AMOUNT][PARA_LENGTH] = {};
+    generate_cv(filtered_cv,included_paragraphs,cv);
+
+    //testing started
+    for(int i = 0; i < PARA_AMOUNT; i++){
+        printf("(%d) density: %lf included?: %d \n",i+1,density_of_paragraph[i],included_paragraphs[i]); //testing
+    }
+    printf("\n");
+    for (int i = 0; i < PARA_AMOUNT; i++) //print input cv.
+    {
+        printf("input CV paragraph [%d] ",i+1);
+        for (int j = 0; j < PARA_LENGTH; j++)
+        {
+            printf("%s ", cv[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < OUTPUT_PARA_AMOUNT; i++) //print output cv.
+    {
+        printf("output CV paragraph [%d] ",i+1);
+        for (int j = 0; j < PARA_LENGTH; j++)
+        {
+            printf("%s ", filtered_cv[i][j]);
+        }
+        printf("\n");
+    }
+    //testing ended
+    
+
+
+    return 0;
+}
--- src/filter.c
+++ src/filter.c
@@ -3,38 +3,55 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef struct {
+    double doubleVal; //density array
+    int intVal; //value to be inserted into cv, to declare which to add first.
+} Tuple;//defines the tuple as (x,y), where x is double val, and y is intval
+
+void include_paragraph();
+bool is_word_match();
+int calculate_paragraph_weight();
+void calculate_paragraph_density();
+void calculate_cv_density();
+int cmp_tuples();
+void generate_cv();
+
 #define KEYWORD_LENGTH 12 //amount of keywords in array
 #define PARA_AMOUNT 6 //amount of paragraphs
 #define PARA_LENGTH 12 //length of each paragraph
 #define MAX_WORDS 10 //max words in the CV
+#define OUTPUT_PARA_AMOUNT 2 //amount of paragraphs in output
 
-typedef struct {
-    double doubleVal; //density array
-    int intVal; //value to be inserted into CV, to declare which to add first.
-} tTuple;//defines the tuple as (x,y), where x is double val, and y is intval
-  
-bool is_word_match();
-int paragraph_Weight();
-void paragraph_Density();
-void cv_Density();
-void inclusion();
-
-//calculates the density of all paragraphs and returns the value into the density array
-void cv_Density(double *density_of_Paragraph, char *CV[PARA_AMOUNT][PARA_LENGTH], char *keyword_List[KEYWORD_LENGTH], int length[PARA_AMOUNT]){
-    for (int i = 0; i < PARA_AMOUNT; i++) //loops through all paragraphs to get each density.
-    {
-        int weight = paragraph_Weight(CV[i],keyword_List,length[i]);
-        paragraph_Density(weight,length[i],density_of_Paragraph, i);
+//checks if a paragraph in CV is apart of "bool included paragraphs" to add those paragraphs to "filtered_cv"
+void generate_cv(char *filtered_cv[OUTPUT_PARA_AMOUNT][PARA_LENGTH], bool included_paragraphs[PARA_AMOUNT], char *cv[PARA_AMOUNT][PARA_LENGTH]){
+    int j = 0;
+    for(int i = 0; i < PARA_AMOUNT; i++){
+        if (included_paragraphs[i] == 1)
+        {
+            for(int k = 0; k < PARA_LENGTH; k++){
+                filtered_cv[j][k] = cv[i][k];
+            }
+            j++;
+        }  
     }
 }
 
-// divies paragraph weight with the same paragraphs length, to find density form 0 to 1
-void paragraph_Density(int Weight, int Length, double* density, int i){
+//calculates the density of all paragraphs and returns the value into the density array
+void calculate_cv_density(double *density_of_paragraph, char *cv[PARA_AMOUNT][PARA_LENGTH], char *keyword_List[KEYWORD_LENGTH], int length[PARA_AMOUNT]){
+    for (int i = 0; i < PARA_AMOUNT; i++) //loops through all paragraphs to get each density.
+    {
+        int weight = calculate_paragraph_weight(cv[i],keyword_List,length[i]);
+        calculate_paragraph_density(weight,length[i],density_of_paragraph, i);
+    }
+}
+
+// divides paragraph weight with the same paragraphs length, to find density form 0 to 1
+void calculate_paragraph_density(int Weight, int Length, double* density, int i){
     density[i] = ((double)Weight)/((double)Length);
 }
 
 //Checks for how many times a paragraph matches keywords
-int paragraph_Weight(char *Paragraph[], char *Keywords[], int length){
+int calculate_paragraph_weight(char *Paragraph[], char *Keywords[], int length){
     int match_Weight = 0;
     for(int j = 0; j < KEYWORD_LENGTH; j++)
     {
@@ -59,31 +76,15 @@ bool is_word_match(char word_1[], char word_2[]){
         return 0;
     }
 }
-  
-// Comparing tuples by comparing the first (double) value = doubleval. to be used in stdlib.qsort
-int cmp_tuples(const void * a, const void * b) {
-    double cmp = ((*(tTuple*)b).doubleVal - (*(tTuple*)a).doubleVal);
-    // Make sure a negative double also results in returning a negative int, and likewise for positive
-    if (cmp < 0.0) {return -1;}
-    else if (cmp > 0.0) {return 1;}
-    else {return 0;}
-}
-
-void inclusion(double *Density, char *CV[PARA_AMOUNT][PARA_LENGTH], int length[PARA_AMOUNT], bool *include) {
-    tTuple priority_array[PARA_AMOUNT]; //defining priority array as a tuple
+//returns a pointer to bool array of which paragraphs that should be included.
+void include_paragraph(double *Density, char *cv[PARA_AMOUNT][PARA_LENGTH], int length[PARA_AMOUNT], bool *include) {
+    Tuple priority_array[PARA_AMOUNT]; //defining priority array as a tuple
 
     for (int i = 0; i < PARA_AMOUNT; i++) { //initializes the priority array with the values from density.
         priority_array[i].doubleVal = Density[i];
         priority_array[i].intVal = i;
-        printf("%d doubleval: %lf, intVal: %d  ---", i, priority_array[i].doubleVal,priority_array[i].intVal); //testing
-        printf("density[%d]: %lf, CV[%d]: %s, length[%d]: %d, include[%d]: %d\n",i, Density[i], i, *CV[i], i, length[i],i,include[i]); //test
     }
-    qsort(priority_array, PARA_AMOUNT, sizeof(tTuple), cmp_tuples); //sorts the priority array from highest density to lowest
-    printf("\n==============\n"); //test
-    for (int i = 0; i < PARA_AMOUNT; i++) //test
-    {
-        printf("%d doubleval: %lf, intVal: %d  ---\n", i, priority_array[i].doubleVal,priority_array[i].intVal); //testing
-    }
+    qsort(priority_array, PARA_AMOUNT, sizeof(Tuple), cmp_tuples); //sorts the priority array from highest density to lowest
 
     int words = 0;
     int i = 0;
@@ -92,5 +93,14 @@ void inclusion(double *Density, char *CV[PARA_AMOUNT][PARA_LENGTH], int length[P
         include[priority_array[i].intVal] = 1; 
         i++;
     } 
+}
+
+// Comparing tuples by comparing the first (double) value = doubleval. to be used in stdlib.qsort
+int cmp_tuples(const void * a, const void * b) {
+    double cmp = ((*(Tuple*)b).doubleVal - (*(Tuple*)a).doubleVal);
+    // Make sure a negative double also results in returning a negative int, and likewise for positive
+    if (cmp < 0.0) {return -1;}
+    else if (cmp > 0.0) {return 1;}
+    else {return 0;}
 }
 
