97e56b4f53234c3833b468f1c3f6a34706630ac6
Mihusu <ming2200@hotmail.com>
2020-11-19 09:53:25 +0100

    Format.c testing

--- src/format.c
+++ src/format.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "format.h"
 
 #define MAX_CHARACTER 100
+void formaliaInLaTeX(char *plaintext);
 
-/*
-The function should convert a plain text into a LaTeX file
-*/
-void LaTeX(char ConLaTeX[MAX_CHARACTER]) {
-    
-    printf("Write down the name of your file");
-    scanf("%c", ConLaTeX[MAX_CHARACTER]);
-
-    if(ConLaTeX[MAX_CHARACTER] <= 0) {
-        printf("could not create a file");
-        exit(EXIT_FAILURE);
-    }
-
-    fopen("%c.tex", ConLaTeX[MAX_CHARACTER]);
+int main(void) {
+    char *filter_text;
+    formaliaInLaTeX(filter_text);
+    return 0;
 }
 
 /*
 This function will make some modification for the plain text like structure, layout, font etc.
+The function should also convert a plain text into a LaTeX file
 */
-int formaliaInLaTeX(preamble, beginText, thePlainText) {
-    
-    LaTeX(thePlainText);
-    preamble;
-    beginText;
 
-    return preamble && beginText;
+void formaliaInLaTeX(char *plaintext) {
+    FILE *fp; 
+    FILE * preamble;
+    FILE * main_tex;
+
+    fp = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\CV.tex", "w"); // Creates a file
+    preamble = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\preliminaries\\preamble.tex", "r");
+    if(preamble == NULL){
+        printf("\nError can't open input file(s)\n"); // temp remove later
+        exit(EXIT_FAILURE);
+    }
+    main_tex = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\main_test.tex", "r");
+    fprintf(fp,"%s %s %s %d", "We", "are", "in", 2012);
 }
 
 /*
 If there is text to one, two, three or more categories then the function will make the
 text more beautiful and readable
 */
+/*
 int contents(education, workExperience, freeText) {
 
     if(education) {
@@ -53,4 +55,4 @@ int contents(education, workExperience, freeText) {
     }
 
     return education && workExperience && freeText;
-}
+}*/
