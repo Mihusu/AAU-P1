795b0c91fbcf01b07ed25f8a667eea453311a366
Mihusu <ming2200@hotmail.com>
2020-11-24 09:46:45 +0100

    Added words

--- src/format.c
+++ src/format.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include ""
-#include "format.h"
 
 #define KEYWORD_LENGTH 12 //amount of keywords in array
-#define PARA_AMOUNT 6 //amount of paragraphs
-#define PARA_LENGTH 12 //length of each paragraph
-#define MAX_WORDS 10 //max words in the CV
-#define OUTPUT_PARA_AMOUNT 2 //amount of paragraphs in output
-#define MAX_CHARACTER 100
-void formaliaInLaTeX(char *plaintext);
+#define PARA_AMOUNT 6     //amount of paragraphs
+#define PARA_LENGTH 12    //length of each paragraph
 
-int main(void){
-    char ***filter_text;
+void formaliaInLaTeX(char *array_text);
+
+int main(void)
+{
+    char *filter_text;
     formaliaInLaTeX(filter_text);
-    
     return 0;
 }
 
@@ -23,25 +19,18 @@ int main(void){
 This function will make some modification for the plain text like structure, layout, font etc.
 The function should also convert a plain text into a LaTeX file
 */
-void formaliaInLaTeX(char ***plaintext) {
-    char *cv[PARA_AMOUNT][PARA_LENGTH] = {{"Jeg","har","en","gym","uddannelse"}, //testing, tb replace by read.c
-                                            {"jeg","har","arbejdet","i","netto"},
-                                            {"Jeg","er","god","til","C","prog"},
-                                            {"Jeg","kan","finde","ud","af","machinelearning","og","statistik","og","sandsynlighedsteori"},
-                                            {"Jeg","tager","en","Bsc","i","Software"},
-                                            {"Erfaring","med","C","python","css","og","databaser"}};
-    char *buzz[KEYWORD_LENGTH] = {"netto","gym","C","prog","css","databaser","python","statistik","sandsynlighedsteori","machinelearning","Bsc","Software"}; //testing, tb replace by read.c
-    FILE * fp; 
-    FILE * preamble;
-    FILE * main_tex;
-    fp = fopen("CV.tex", "w"); // Creates a file
-    preamble = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\preliminaries\\preamble.tex", "r");
-    if(preamble == NULL){
+void formaliaInLaTeX(char *array_text)
+{
+    FILE *fp;
+    FILE *preamble;
+    FILE *main_tex;
+    fp = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\CV.tex", "w+"); // Creates a file
+    if (preamble == NULL)
+    {
         printf("\nError can't open input file(s)\n"); // temp remove later
         exit(EXIT_FAILURE);
     }
-    main_tex = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\main_test.tex", "r");
-    fprintf(fp,"%s %s %s %d", "We", "are", "in", 2012);
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2012);
 }
 
 /*
