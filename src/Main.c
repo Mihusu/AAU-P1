6ad88b1077b113477a31cdfb1d6499a3beb05a82
Mihusu <ming2200@hotmail.com>
2020-11-25 12:30:28 +0100

    Added a lot of things

--- src/Main.c
+++ src/Main.c
@@ -4,19 +4,22 @@
 #include <string.h>
 #include "filter.c"
 #include "testingRead.c" //testing to be replaced by full version
-//#include "format.c"
+#include "format.c"
 
 int main(void){
     printf("########  ########   #######        ## ########  ######  ########    ##     ## #### ########    ###\n##     ## ##     ## ##     ##       ## ##       ##    ##    ##       ##     ##  ##     ##      ## ##   \n##     ## ##     ## ##     ##       ## ##       ##          ##       ##     ##  ##     ##     ##   ##  \n########  ########  ##     ##       ## ######   ##          ##       ##     ##  ##     ##    ##     ## \n##        ##   ##   ##     ## ##    ## ##       ##          ##        ##   ##   ##     ##    ######### \n##        ##    ##  ##     ## ##    ## ##       ##    ##    ##         ## ##    ##     ##    ##     ## \n##        ##     ##  #######   ######  ########  ######     ##          ###    ####    ##    ##     ## \n");
     //wordsinsections tells how many words in each paragraph/section. section count is total section numbers.
     int sectionsCount, *wordsInSections, i, j;
     char *fullText, ***theSectionsOut;
+    char *filtered_cv; //makes a dynamic variable to later be malloced to be used to dynamically change length/words in new cv
     FILE *theFileIn = fopen("Long.txt", "r");
+    FILE *CV_education_and_work_experience = fopen("LongCV.txt", "r");
     text_reader(theFileIn, &fullText);
     printf("\nchars total: %d\n", strlen(fullText));
     section_treater(fullText, &theSectionsOut, &sectionsCount, &wordsInSections);
     printf("Number of sections: %d\n", sectionsCount);
-    //===============================================read ^^ filter vv =============================0
+    
+    /*============================ read ^^ filter vv =============================*/
     char *buzz[KEYWORD_LENGTH] = {"elektronik","c#","java","gym","c","prog","css","databaser","python","statistik","sandsynlighedsteori","machinelearning","bsc","software"}; //testing, tb replace by read.c
 
     double *density_of_paragraph = calloc(sectionsCount,sizeof(double)); //defines a density for each sections/paragraph
@@ -24,32 +27,13 @@ int main(void){
     bool *included_paragraphs = calloc(sectionsCount,sizeof(bool)); //defines an array of which paragraphs/sections should be included
     include_paragraph(density_of_paragraph,theSectionsOut,wordsInSections,included_paragraphs,sectionsCount);
     for (int i = 0; i < sectionsCount; i++){
-        printf("(%d). density: %lf, include? %d\n",i,density_of_paragraph[i],included_paragraphs[i]);
+        printf("(%d). density: %lf, include %d\n",i,density_of_paragraph[i],included_paragraphs[i]);
     }
-    
-    char *filtered_cv; //makes a dynamic variable to later be malloced to be used to dynamically change length/words in new cv
     generate_cv(&filtered_cv,included_paragraphs,theSectionsOut,sectionsCount,wordsInSections);
     
-    printf("\nOutput: %s\n", filtered_cv);
-    FILE *fp = fopen("CV.tex", "w"); // Creates a file
-   
-    if (fp == NULL) { 
-        printf("Cannot open file \n"); 
-        exit(EXIT_FAILURE); 
-    } 
-
-    //fgets(filtered_cv, sizeof(filtered_cv), stdin);
-    fprintf(fp, "output: %s\n", filtered_cv);
-    fclose(fp);
-
-    /*char received_string[20000]; 
-    for (i = 0; filtered_cv[i]!='\0'; i++) {
-        // Input string into the file 
-        // single character at a time 
-        fputc(filtered_cv[i], fp); 
-    } 
-    // Reading the string from file 
-    fgets(received_string, 20000, fp); */
+    printf("\n%s\n", filtered_cv);
+    output_LaTeX_free_text(filtered_cv);
+    //output_LaTeX_essential_contents(general_information, education, work_experience);   // SKAL BRUGES. M.I.S
 
     //testing read started again, to free variables
     free(filtered_cv); //freeinng variables
--- src/format.c
+++ src/format.c
@@ -1,53 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "filter.c"
+#include <ctype.h> 
 
-void formaliaInLaTeX(char *array_text);
+void output_capitalized(char *capitalized_text) {
+    //Calculating length.
+    int marker_index = -1;
+    int length = strlen(capitalized_text);
+    *capitalized_text = toupper(*capitalized_text);
+    for(int i = 1; i < length-1; i++) {
+        if(marker_index == i && (capitalized_text[i] == '\n' || capitalized_text[i] == ' ')) {
+            capitalized_text[i+1] = toupper(capitalized_text[i+1]);
+        } else if(capitalized_text[i] == '.' || capitalized_text[i] == '?' || capitalized_text[i] == '!') {
+            marker_index = i + 1;
+        }
+    }
+}
 
-int main(void)
-{
-    char *filter_text;
-    formaliaInLaTeX(filter_text);
-    return 0;
+/*
+If there is text to one, two, three or more categories then the function will make the
+text more beautiful and readable.
+*/
+void output_LaTeX_essential_contents(char *array_general_information, char ***array_essential_contents) {
+    FILE *CV_contents = fopen(".\\..\\docs\\CV.tex", "w");
+    if (CV_contents == NULL) { 
+        printf("Cannot open file\n"); 
+        exit(EXIT_FAILURE);
+    }
 }
 
 /*
 This function will make some modification for the plain text like structure, layout, font etc.
-The function should also convert a plain text into a LaTeX file
+The function should also convert a plain text into a LaTeX file.
 */
-void formaliaInLaTeX(char *array_text)
-{
-    FILE *fp;
-    char c;
-    fp = fopen("D:\\Skole\\Universitet\\1_semester\\P1\\AAU_P1\\docs\\CV.tex", "w+"); // Creates a file 
-    fprintf(fp, "%s", "hej");
+void output_LaTeX_free_text(char *array_free_text) {
+    FILE *CV_free_text = fopen(".\\..\\docs\\CV.tex", "w"); // Creates a file.
+    if(CV_free_text == NULL) { 
+        printf("Cannot open file\n"); 
+        exit(EXIT_FAILURE); 
+    }
 }
 
-void output(char *array_text) {
-
+void run_pdfLaTeX(char *run_general_info, char ***run_essential_info, int n, int *amount_of_itemized, char *run_free_text) {
+    FILE *final_CV = fopen(".\\..\\docs\\CV.tex", "w");
+    if(final_CV == NULL) { 
+        printf("Cannot open file\n"); 
+        exit(EXIT_FAILURE); 
+    }
+    output_capitalized(run_general_info);
+    fprintf(final_CV,"\\section{General information}\n%s\n", run_general_info);
+    for(int i = 0; i < n; i++) {
+        fprintf(final_CV,"\\section{}\n%s\n", );
+    }
+    output_capitalized(run_free_text);
+    // Prints out the free text that has been filtered to the CV.tex file.
+    fprintf(final_CV,"\\section{Free text}\n%s\n", run_free_text);
+    fclose(final_CV);
 }
-/*
-If there is text to one, two, three or more categories then the function will make the
-text more beautiful and readable
-*/
-/*
-int contents(education, workExperience, freeText) {
-
-    if(education) {
-        // Make bulletpoints for the text
-        printf("* ");
-    }
-    
-    if(workExperience) {
-        // Make bulletpoints for the text
-        printf("* ");
-
-    }
-
-    if(freeText) {
-
-    }
-
-    return education && workExperience && freeText;
-}*/
