a36861dbeb9e2653bac769f4082781d11dc6d01d
Mihusu <ming2200@hotmail.com>
2020-12-07 10:13:08 +0100

    Added new things to product_development

--- src/format.c
+++ src/format.c
@@ -5,19 +5,25 @@
 
 /*
 This program will check the format and edited it accordingly to how a CV should be visualized. 
-There are included a LaTeX format to enhance readability for the user and recruit when reading it
-to the program. A picture af yourselves is required for most of the jobs that are availiable.
+There are included a LaTeX format to enhance readability for the user and recruiter,
+and if there is information about either the education or work experience
+then the program will automatically format it self into bullet points text.
+A picture of yourselves is required for most of the jobs that are available.
 This program will at last print out a pdf-file for the user. 
 */
+
 void output_capitalized(char *capitalized_text) {
+    // Two markers that are temporary out of the index
     int marker_index_1 = -1;
     int marker_index_2 = -1;
+    
     //Calculating length.
     int length = strlen(capitalized_text);
     // Capitalized the first paragraph for the first letter with toupper
     *capitalized_text = toupper(*capitalized_text);
+
     // This will capitalize for other scenarios
-    for(int i = 1; i < length - 1; i++) { // length should go -1 because of the '.'
+    for(int i = 1; i < length - 1; i++) { // length are going down with -1 because it must stop at '.'
         if(marker_index_1 == i) {
             if(capitalized_text[i] == ' ') {
                 // Capitalize the next letter
@@ -46,14 +52,17 @@ void output_general_contents(char *array_general_information, FILE *general_CV) 
     fprintf(general_CV,"\\begin{center}\n \\begin{minipage}[b]{0.45\\textwidth} \n\\subsection*{General information}\n%s\\newline\n \\end{minipage}\n \\hfill", array_general_information);
 }
 
-/* Load a picture to the CV. That can be essential for the recruiter. */
+/* 
+Load a picture to the CV. That can be essential for the recruiter.
+This will also set the picture at the right side of the CV.
+*/
 void output_picture(FILE *picture) {
     //Put your picture here after the phrase figures/...
     // This figure can be adjusted, so it will be at the right side of the CV
     fprintf(picture, "\n\\begin{minipage}[b]{4.5cm}\n \\includegraphics[height=4.25cm]{figures/Billede_af_David}\n \\end{minipage}\n \\end{center}\n\n");
 }
 
-void output_essential_contens(char ***array_essential_contents, int amount_of_itemizes, int *amount_of_items_in_a_itemize, FILE *essenctial_CV) {
+void output_essential_contents(char ***array_essential_contents, int amount_of_itemizes, int *amount_of_items_in_a_itemize, FILE *essenctial_CV) {
     for(int i = 0; i < amount_of_itemizes; i++) {
         // The print is going to start at index 0, because of 'i' is going to start at 0.
         fprintf(essenctial_CV, "\\section*{%s}\n\\begin{itemize}\n", array_essential_contents[i][0]);
@@ -74,17 +83,18 @@ void output_LaTeX_free_text(char *array_free_text, FILE *free_text_CV) {
     fprintf(free_text_CV,"\n\\section*{Background Information}\n%s\n", array_free_text);
 }
 
-void run_pdfLaTeX(char *run_general_info, char ***run_essential_info, int n, int *amount_of_itemized_to_g_info_and_essential_info, char *run_free_text) {
+void run_pdfLaTeX(char *run_general_info, char ***run_essential_info, int itemizes, int *items_for_G_info_and_essential_info, char *run_free_text) {
     // Create or overwrite a LaTeX file, and check it can create or overwrite it.
     FILE *final_CV;
     final_CV = fopen(".\\..\\docs\\CV.tex", "w");
+    
     if(final_CV == NULL) { 
         printf("Cannot open file\n"); 
         exit(EXIT_FAILURE); 
     }
     output_general_contents(run_general_info, final_CV);
     output_picture(final_CV);
-    output_essential_contens(run_essential_info, n, amount_of_itemized_to_g_info_and_essential_info, final_CV);
+    output_essential_contents(run_essential_info, itemizes, items_for_G_info_and_essential_info, final_CV);
     output_LaTeX_free_text(run_free_text, final_CV);
     fclose(final_CV);
 }
