dd1549154e23e2ef553e507ea5693a9c55fa71d7
Mihusu <ming2200@hotmail.com>
2020-12-08 18:57:31 +0100

    Added h files, snake case read.c, picture edited

--- src/filter.c
+++ src/filter.c
@@ -1,28 +1,10 @@
+#include "filter.h"
+
 /*
 This program filters the input CV, so that only appropiate parts are included, and the parts
 that are included are shortened if some parts are unnecessary.
 The output of this file will be used in the format.c file.
 */
-
-typedef struct {
-    double doubleVal; //density array
-    int intVal; //value to be inserted into cv, to declare which to add first.
-} Tuple;//defines the tuple as (x,y), where x is double val, and y is intval
-
-#define MAX_WORDS 110 //max words in the CV
-#define PP_AMOUNT 62 //amount of personal pronouns.
-
-void include_section();
-bool is_word_match();
-int calculate_section_weight();
-void calculate_section_density();
-void calculate_text_density();
-int cmp_tuples();
-void generate_text();
-void remove_punctuation();
-void remove_duplicate();
-int levenshtein();
-void remove_personal_pronouns();
 
 //removes all personal pronouns of the start of every sentence of the input section, before calculating cv density
 void remove_personal_pronouns(int *words_in_sections,int sections_count,char ***sections_out){
--- src/filter.h
+++ src/filter.h
@@ -1,26 +1,24 @@
 #ifndef P1_SRC_FILTER_H
 #define P1_SRC_FILTER_H
 
-#define KEYWORD_LENGTH 12  // amount of keywords in array
-#define PARA_AMOUNT 6      // amount of paragraphs
-#define PARA_LENGTH 12     // length of each paragraph
+typedef struct {
+    double doubleVal; //density array
+    int intVal; //value to be inserted into cv, to declare which to add first.
+} Tuple;//defines the tuple as (x,y), where x is double val, and y is intval
 
-// calculates the density of all paragraphs and returns the value into the
-// density array
-void cv_Density(double *density_of_Paragraph,
-                char *CV[PARA_AMOUNT][PARA_LENGTH],
-                char *keyword_List[KEYWORD_LENGTH], int length[PARA_AMOUNT]);
-// divies paragraph weight with the same paragraphs length, to find density form
-// 0 to 1
-void paragraph_Density(int Weight, int Length, double *density, int i);
-// Checks for how many times a paragraph matches keywords
-int paragraph_Weight(char *Paragraph[], char *Keywords[], int length);
-// compares for string match, to see if they are the same.
-bool is_word_match(char word_1[], char word_2[]);
-// Comparing tuples by comparing the first (double) value = doubleval. to be
-// used in stdlib.qsort
-int cmp_tuples(const void *a, const void *b);
-void inclusion(double *Density, char *CV[PARA_AMOUNT][PARA_LENGTH],
-               int length[PARA_AMOUNT], bool *include);
+#define MAX_WORDS 375 //max words in the CV
+#define PP_AMOUNT 62 //amount of personal pronouns.
+
+void include_section();
+bool is_word_match();
+int calculate_section_weight();
+void calculate_section_density();
+void calculate_text_density();
+int cmp_tuples();
+void generate_text();
+void remove_punctuation();
+void remove_duplicate();
+int levenshtein();
+void remove_personal_pronouns();
 
 #endif  // P1_SRC_FILTER_H
